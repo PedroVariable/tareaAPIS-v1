3d4b18c61191f0b56ceeff7aa5206e9f5b6884fe
PedroVariable <pedro.morales1198@gmail.com>
2024-06-29 10:00:47 -0600

    first commit

---
+++ Models/Dbtareas901Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace tareaAPIS_v1.Models;
+
+public partial class Dbtareas901Context : DbContext
+{
+    public Dbtareas901Context()
+    {
+    }
+
+    public Dbtareas901Context(DbContextOptions<Dbtareas901Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-V6J35G2\\MSSQLSERVER01;Initial Catalog=DBtareas_901;user id=sa;password=1234;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea).HasName("PK__tarea__756A5402D9D093C6");
+
+            entity.ToTable("tarea");
+
+            entity.Property(e => e.IdTarea).HasColumnName("idTarea");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tareaAPIS_v1.Models;
+
+public partial class Tarea
+{
+    public int IdTarea { get; set; }
+
+    public string? Nombre { get; set; }
+}
